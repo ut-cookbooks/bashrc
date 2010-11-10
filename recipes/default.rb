625f87b9bd1c29bf9b46beb7e6f15320e7e2f536
Fletcher Nichol <fnichol@nichol.ca>
2010-11-10 11:22:47 -0700

    Metadata updates.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: bashrc
 # Recipe:: default
 #
-# Copyright 2010, Example Com
+# Copyright 2010, Fletcher Nichol
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
