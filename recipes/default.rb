47a95e6d0e8dd38a7fde1feaad6866937744beb2
fnichol <fnichol@nichol.ca>
2011-02-13 21:09:46 -0700

    Add a bash-source short circuit under Ubuntu to allow prompts being set.

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,3 +34,29 @@ bash "install_bashrc" do
   creates "/etc/bash/bashrc"
 end
 
+if platform?("ubuntu") 
+  # shortcut the sourcing of ${HOME}/.profile which on Ubuntu sources
+  # ${HOME}/.bashrc which sets prompts (again!) and other undesirables
+  file "/etc/skel/.bash_login" do
+    owner   "root"
+    group   "root"
+    mode    "0644"
+    action  :create
+  end
+
+  file "/root/.bash_login" do
+    owner   "root"
+    group   "root"
+    mode    "0644"
+    action  :create
+  end
+
+  # support for vagrant user if using vagrant
+  file "/home/vagrant/.bash_login" do
+    owner   "root"
+    group   "root"
+    mode    "0644"
+    action  :create
+    only_if %{test -d /home/vagrant}
+  end
+end
