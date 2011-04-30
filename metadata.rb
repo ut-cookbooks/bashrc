635f56da82ae0e68da901da56bdc057919cd2f46
Fletcher Nichol <fnichol@nichol.ca>
2011-04-30 00:12:25 -0600

    Bump to 0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "fnichol@nichol.ca"
 license           "Apache 2.0"
 description       "Installs/Configures fnichol/bashrc"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1"
+version           "0.2.0"
 recipe            "bashrc", "Installs github.com/fnichol/bashrc bash profile"
 
 %w{ ubuntu suse }.each do |os|
