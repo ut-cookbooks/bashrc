3d06fafd765b0528980c3a751f7c1da1226bc64f
Fletcher Nichol <fnichol@nichol.ca>
2011-04-30 00:17:13 -0600

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "fnichol@nichol.ca"
 license           "Apache 2.0"
 description       "Installs/Configures fnichol/bashrc"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.2.0"
+version           "0.2.1"
 recipe            "bashrc", "Installs github.com/fnichol/bashrc bash profile"
 
 %w{ ubuntu suse }.each do |os|
@@ -13,3 +13,13 @@ end
 %w{ git }.each do |cb|
   depends cb
 end
+
+attribute "bashrc/update",
+  :display_name => "Update bashrc on each Chef run?",
+  :description  => "Whether or not to update *bashrc* on every Chef execution.",
+  :default      => false
+
+attribute "bashrc/install_url",
+  :display_name => "URL for installer script",
+  :description  => "The URL containing the installer script.",
+  :default      => "http://bit.ly/bashrc-install-system-wide"
