f087c77a9d6c623ba3be1a39fa703d6ac230d676
Fletcher Nichol <fnichol@nichol.ca>
2011-09-25 11:20:06 -0600

    Whitspace cleanup.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,7 +37,7 @@ execute "install_bashrc" do
   creates   "/etc/bash/bashrc"
 end
 
-if platform?("ubuntu") 
+if platform?("ubuntu")
   # shortcut the sourcing of ${HOME}/.profile which on Ubuntu sources
   # ${HOME}/.bashrc which sets prompts (again!) and other undesirables
   file "/etc/skel/.bash_login" do
