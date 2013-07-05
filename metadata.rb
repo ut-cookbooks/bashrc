f730743bcecba466ed74a7afaa22f1423cbc41fc
Fletcher Nichol <fnichol@nichol.ca>
2013-07-05 10:51:05 -0600

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "fnichol@nichol.ca"
 license           "Apache 2.0"
 description       "Installs a bash profile from http://github.com/fnichol/bashrc."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.4"
+version           "0.3.5"
 
 supports "ubuntu"
 supports "suse"
