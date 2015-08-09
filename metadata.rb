1d88d620aec9a9a1364cd00fd209c87b340815d6
Fletcher Nichol <fnichol@nichol.ca>
2015-08-09 14:08:27 -0600

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Fletcher Nichol"
 maintainer_email  "fnichol@nichol.ca"
 license           "Apache 2.0"
 description       "A resource to install a bash profile from http://github.com/fnichol/bashrc."
-version           "2.0.4"
+version           "2.0.5"
 
 supports "centos"
 supports "mac_os_x"
